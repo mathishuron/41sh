11ea5270ce71be4123a359110066f4df33cb6aa9
mlko <flashcpm@gmail.com>
2025-09-10 14:49:47 +0200

    xor

--- src/main.c
+++ src/main.c
@@ -1,5 +1,38 @@
 #include "../include/libc/libc.h"
 
+// Tout bon, entrée chifrée et get déchifré, ne reste plus qu'à decipher le list
+
+char 	*xor_encrypt_decrypt(char *text, char *key)
+{
+    int i;
+    int j;
+    int text_len;
+    int key_len;
+    char *result;
+    
+    text_len = my_strlen(text);
+    key_len = my_strlen(key);
+    
+    result = (char *)my_malloc((text_len + 1) * sizeof(char));
+    
+    i = 0;
+    j = 0;
+    while (i < text_len)
+    {
+        result[i] = text[i] ^ key[j];
+        
+        i++;
+        j++;
+        
+        if (j >= key_len)
+            j = 0;
+    }
+    
+    result[i] = '\0';
+    
+    return (result);
+}
+
 char	*get_base(char *file)
 {
 	int	fd;
@@ -17,8 +50,39 @@ char	*get_base(char *file)
 	}
 	bytes_read = my_getfile(buffer, buffer_size, fd);
 	my_close(fd);
+	// char *plain = xor_encrypt_decrypt(buffer[0], file);
 	return (buffer[0]);
 
+}
+
+void decipher_base(char *file)
+{
+    char *buffer = get_base(file);
+    int i = 0;
+    while (buffer[i] != '\0') {
+        // Affiche le nom du site (jusqu'à l'espace)
+        while (buffer[i] != ' ' && buffer[i] != '\0') {
+            my_putchar(buffer[i]);
+            i++;
+        }
+        if (buffer[i] == ' ') {
+            my_putchar(' ');
+            i++;
+        }
+        int start = i;
+        while (buffer[i] != '\n' && buffer[i] != '\0')
+            i++; // chaque char du pass
+        if (i > start) {
+            char temp = buffer[i];
+            buffer[i] = '\0'; 
+            char *plain = xor_encrypt_decrypt(buffer + start, file);
+            my_printf("%s", plain);
+            buffer[i] = temp;
+        }
+        if (buffer[i] == '\n') {
+            i++;
+        }
+    }
 }
 
 char	*retrieve_password(char *file, char *entry)
@@ -26,7 +90,7 @@ char	*retrieve_password(char *file, char *entry)
 	int	fd;
 	int	i;
 	int	j;
-	int	size_password;
+	int	size_password = 0;
 	int	len_entry;
 	char	*buffer[1];
 	char	*password;
@@ -51,12 +115,14 @@ char	*retrieve_password(char *file, char *entry)
 				j++;
 			}
 			password[j] = '\0';
-			return (password);
+			char *plain = xor_encrypt_decrypt(password, file);
+			return (plain);
 		}
 		i++;
 	}
 	return (0);
 }
+
 
 void	add_entry(char *file, char *entry)
 {
@@ -64,7 +130,6 @@ void	add_entry(char *file, char *entry)
 	int	bytes_read;
 	char	*buffer[1];
 	my_size_t	buffer_size[1];
-	
 	if (retrieve_password(file, entry) == 0)
 	{
 		fd = my_open(file, O_APPEND| O_WRONLY);
@@ -79,12 +144,14 @@ void	add_entry(char *file, char *entry)
 		buffer_size[0] = BUFFER_LEN;
 		my_printf("Enter password for entry %s : ", entry);
 		bytes_read = my_getline(buffer, buffer_size, 0);
-		my_write(fd, buffer[0], bytes_read);
+		char *cipher = xor_encrypt_decrypt(buffer[0], file);
+		my_write(fd, cipher, my_strlen(cipher));
+		my_write(fd, "\n", 1);
 		my_printf("Saved successfully.\n");
 		my_close(fd);
 	}
 	else
-		my_printf("Error : entry already exists\n");
+		my_printf("Error : You already have a pass for this website\n");
 
 	return;
 }
@@ -105,7 +172,9 @@ void	check_password(char *file, char **password, my_size_t *password_size)
 	my_printf("Enter master password : ");
 	my_getline(password, password_size, 0);
 	db_pass = retrieve_password(file, "master-password");
-	if (db_pass != 0 && my_strncmp(db_pass, password[0], my_strlen(db_pass)) == 0)
+	int len = my_strlen(password[0]);
+
+	if (my_strncmp(db_pass, password[0], my_strlen(password[0])) == 0)
 		return;
 	else
 	{
@@ -114,6 +183,7 @@ void	check_password(char *file, char **password, my_size_t *password_size)
 		my_exit(1);
 	}
 }
+
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -128,7 +198,7 @@ int	main(int argc, char **argv, char **envp)
 		if (my_strcmp(argv[1], "get") == 0)
 		{
 			if (argc < 4)
-				my_printf("Not enough argument for get\n");
+				my_printf("Bad usage, use \"get <vault_name> <site>\"\n");
 			else
 			{
 				check_password(argv[2], password, password_size);
@@ -141,17 +211,17 @@ int	main(int argc, char **argv, char **envp)
 		else if (my_strcmp(argv[1], "list") == 0)
 		{
 			if (argc < 3)
-				my_printf("Missing DB name for list");
+				my_printf("Bad usage, use \"list <vault_name>\"\n");
 			else
 			{
 				check_password(argv[2], password, password_size);
-				my_printf(get_base(argv[2]));
+				decipher_base(argv[2]);
 			}
 		}
 		else if (my_strcmp(argv[1], "add") == 0)
 		{
 			if (argc < 4)
-				my_printf("Not enough arguments for add");
+				my_printf("Bad usage, use \"add <vault_name> <site>\"\n");
 			else
 			{
 				check_password(argv[2], password, password_size);
@@ -161,15 +231,21 @@ int	main(int argc, char **argv, char **envp)
 		else if (my_strcmp(argv[1], "init") == 0)
 		{
 			if (argc < 3)
-				my_printf("Missing database name\n");
+				my_printf("Bad usage, use \"init <vault_name>\"\n");
 			else
 				init_base(argv[2]);
+		}
+		else if (my_strcmp(argv[1], "test") == 0)
+		{
+			test();
 		}
 
 	}
 	else
 	{
-		my_printf("Not enough arguments\n");
+		my_printf("Usage: %s <command> [args...]\n", argv[0]);
+		my_printf("Commands: init, add, list, get, test\n");
+		return 1;
 	}
 	my_free();
 	return (0);
